f6e14d074f27013338a8b93c520b38f81f258a96
phea3 <sokphealoun9@gmail.com>
2023-07-13 15:21:14 +0700

    Initial Commit

---
+++ Task ManagementApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Task ManagementApp
+//
+//  Created by loun sokphea on 13/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Task ManagementApp/Task_ManagementAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_ManagementAppApp.swift
+//  Task ManagementApp
+//
+//  Created by loun sokphea on 13/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Task_ManagementAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
